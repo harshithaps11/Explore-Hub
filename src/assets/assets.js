0346c7913c38496513255872d785938dabd1dda2
harshitha <harshithapremkumar11@gmail.com>
2024-11-12 17:46:29 +0530

    files created and navbar components added

---
+++ src/assets/assets.js
@@ -0,0 +1,15 @@
+import logo from './logo.png'
+import logo1 from './logo1.png'
+import logo2 from './logo2.png'
+import logo3 from './logo3.png'
+import logo4 from './logo4.png'
+import react from './react.svg'
+
+export const assets = {
+    logo,
+    logo1,
+    logo2,
+    logo3,
+    logo4,
+    react,
+}
