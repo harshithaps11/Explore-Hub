911d41f41f138f8d4244ba540800d25edb635551
harshitha <harshithapremkumar11@gmail.com>
2024-11-15 10:13:10 +0530

    New components added and updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Selects from './components/Selects'
+import SelectsCard from './components/SelectsCard'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Navbar/>
+      <Hero/>
+      <Selects/>
+      <SelectsCard/>
+      <Carousel/>
+      <Footer/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,64 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FaBars, FaFacebook,FaTwitter,FaYoutube, FaInstagram, FaPinterest, FaSearch } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+import { HiOutlineMenuAlt4 } from 'react-icons/hi';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [logo, setLogo] = useState(false)
+  const handleNav = () => {
+    setNav(!nav);
+    setLogo(!logo)
+  };
+
   return (
-    <div className='flex justify-between items-center h-20'>
-        <div>
-            <h1>BEACHES.</h1>
+    <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white'>
+        <div >
+            <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>EXPLORE HUB</h1>
         </div>
-        <ul>
+        <ul className='hidden md:flex'>
             <li>Home</li>
             <li>Destination</li>
             <li>Travel</li>
             <li>View</li>
             <li>Book</li>
         </ul>
+        <div className='hidden md:flex'>
+        <FaBars className='mr-2' size={24} />
+        <FaSearch size={20} />
+        </div>
+
+        {/*Hamburger*/}
+        <div onClick={handleNav} className='md:hidden z-10'>
+          {nav ? <AiOutlineClose className='text-black' size={20} /> : <HiOutlineMenuAlt4 size={24}/>}
+          
+        </div>
+
+        {/*Mobile menu dropdown*/}
+        <div onClick={handleNav} className={nav ? 'absolute text-black  left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+          <ul>
+            <h1>EXPLORE HUB</h1>
+            <li className='border-b'>Home</li>
+            <li className='border-b'>Destination</li>
+            <li className='border-b'>Travel</li>
+            <li className='border-b'>View</li>
+            <li className='border-b'>Book</li>
+            <div className='flex flex-col'>
+              <button className='my-6'>Search</button>
+              <button>Account</button>
+            </div>
+            <div className='flex justify-between my-6'>
+              <FaFacebook className='icon'/>
+              <FaTwitter className='icon'/>
+              <FaYoutube className='icon'/>
+              <FaPinterest className='icon'/>
+              <FaInstagram className='icon'/>
+            </div>
+          </ul>
+        </div>
     </div>
+
   )
 }
 
-export default Navbar
+export default Navbar;
